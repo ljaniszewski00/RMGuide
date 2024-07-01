ed0c47294c8f1c2da6dc2692d2f37fe4d2975173
Łukasz Janiszewski <ljaniszewski00@gmail.com>
2024-07-01 19:11:03 +0200

    Initial Commit

---
+++ RMGuide/RMGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMGuideApp.swift
+//  RMGuide
+//
+//  Created by Łukasz Janiszewski on 01/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct RMGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
