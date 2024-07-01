fb5e85b3655419ba9b1358cfebdb576e8ed15bb9
Łukasz Janiszewski <ljaniszewski00@gmail.com>
2024-07-01 19:28:52 +0200

    Added app modules file structure

---
+++ RMGuide/EpisodeDetails/View/EpisodeDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  EpisodeDetailsView.swift
+//  RMGuide
+//
+//  Created by Łukasz Janiszewski on 01/07/2024.
+//
+
+import SwiftUI
+
+struct EpisodeDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EpisodeDetailsView()
+}
